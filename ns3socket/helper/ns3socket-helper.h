f66d7a58bc57dee2d89b920d84efa0e1f9e53d73
muzhi009 <20201118009@nuist.edu.cn>
2025-05-04 09:52:38 +0800

    Add files via upload

---
+++ ns3socket/helper/ns3socket-helper.h
@@ -0,0 +1,14 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef NS3SOCKET_HELPER_H
+#define NS3SOCKET_HELPER_H
+
+#include "ns3/ns3socket.h"
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* NS3SOCKET_HELPER_H */
+
